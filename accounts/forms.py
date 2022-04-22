52e62245b5138dddd5ad1cbd2d2050e26aafff38
Tranmyhan <tranmyhan123@gmail.com>
2022-04-22 10:55:09 +0700

    part 17

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,8 +5,13 @@ from django.contrib.auth.models import User
 from django import forms
 
 
-from .models import Order
+from .models import Customer, Order
 
+class CustomerForm(ModelForm):
+	class Meta:
+		model = Customer
+		fields = '__all__'
+		exclude = ['user']
 
 class OrderForm(ModelForm):
 	class Meta:
