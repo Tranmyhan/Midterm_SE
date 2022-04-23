154f6d30632f34310d66635039e497120813eb85
Tranmyhan <tranmyhan123@gmail.com>
2022-04-23 10:21:42 +0700

    part 20

--- crm1/settings.py
+++ crm1/settings.py
@@ -126,3 +126,13 @@ STATICFILES_DIRS = [
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+
+
+#SMTP Configuration
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = '***' #Điền email của host
+EMAIL_HOST_PASSWORD = '***' #Điền password email của host
